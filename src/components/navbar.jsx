d09959dcc16784a6288e3630afdc50d2ecc3cc62
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-14 16:33:29 -0600

    v0.08: Sorted out tailwind live build issues. Turned out I didn't install tailwind properly, but somehow it was working in my IDE. Creating header

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
         <div className='absolute top-0 left-0 w-full z-10'>
             <div className='container mx-auto flex justify-between
             items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
-                <img alt={"button"} src={icon} />
-                <ul>
-                    <a href="#Header" className="cursor-pointer hover:text-gray-400 underline-none !important">Home</a>
+                <img alt="" src={icon} />
+                <ul className="hidden md:flex gap-7 text-white">
+                    <a href="#Header" className="cursor-pointer hover:text-gray-400 underline-none">Home</a>
                     <a href="#Header" className="cursor-pointer hover:text-gray-400 no-underline">About</a>
                     <a href="#Header" className="cursor-pointer hover:text-gray-400 no-underline">Projects</a>
                     <a href="#Header" className="cursor-pointer hover:text-gray-400 no-underline">Testimonials</a>
