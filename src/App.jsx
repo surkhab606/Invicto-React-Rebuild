15a12f3a98128eb6ae1601c7eb86141d3d92543a
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-14 16:17:12 -0600

    v0.07: Basic navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from "./components/navbar.jsx";
 
 const App = () => {
     return (
         <div>
-
+            <Navbar/>
         </div>
     )
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import icon from '../assets/logo.svg'
 
-const App = () => {
+const Navbar = () => {
     return (
         <div className='absolute top-0 left-0 w-full z-10'>
-
+            <div className='container mx-auto flex justify-between
+            items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent'>
+                <img alt={"button"} src={icon} />
+                <ul>
+                    <a href="#Header" className="cursor-pointer hover:text-gray-400 underline-none !important">Home</a>
+                    <a href="#Header" className="cursor-pointer hover:text-gray-400 no-underline">About</a>
+                    <a href="#Header" className="cursor-pointer hover:text-gray-400 no-underline">Projects</a>
+                    <a href="#Header" className="cursor-pointer hover:text-gray-400 no-underline">Testimonials</a>
+                </ul>
+                <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
+            </div>
         </div>
     )
 }
 
-export default App
+export default Navbar
