6abab2970c1db1f118fd67242f7faed2f4eca229
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-14 18:38:48 -0600

    v0.10: Formatted splash text on homepage, set font from Google Fonts and set it to be wide. Created two buttons as well for quick access to Projects & Contact

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,8 +6,14 @@ const Header = () => {
         <div className='min-h-screen mb-4 bg-cover bg-center flex
         items-center w-full overflow-hidden' style={{backgroundImage: "url('./header_img.jpg')"}} id='Header'>
             <Navbar/>
-            <div>
-                <h2>Build Your Dream Home with Calgary’s Trusted Experts</h2>
+            <div className='container text-center mx-auto py-4 px-6 md:px-20 lg:px-32
+            text-white '>
+                <h2 className='text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl
+                font-semibold pt-20 tracking-wide'>Your Vision, Our Expertise</h2>
+                <div>
+                    <a href="">Explore More</a>
+                    <a href="">Contact Us</a>
+                </div>
             </div>
         </div>
     )
