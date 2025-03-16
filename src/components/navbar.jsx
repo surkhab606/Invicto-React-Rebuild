7ef1a14ec0b8e47011d051814429cdea7ea4975a
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-16 00:47:25 -0600

    v0.13: Mobile formatting is complete. The open and close buttons work perfectly and there is even a bit of default animation that I appreciate! We used the useState hook for showing and setting the mobile menu, and yes, it is accessible and fully functioning on desktop if you minimize the width of the website as much as possible! Then, we have a useEffect for mobile menu scrolling before the website loads to prevent any weird bugs.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useState}, {useEffect()} from 'react'
 import icon from '/headerlogo.png'
 import menuIcon from '/menu_icon.svg'
 import crossIcon from '/cross_icon.svg'
+import {useEffect} from "react";
 
 const Navbar = () => {
+    const [showMobileMenu, setShowMobileMenu] = useState(false)
+
+    useEffect (() => {
+        if(showMobileMenu) {
+            document.body.style.overflow = 'hidden'
+        }
+    }, [showMobileMenu])
     return (
         <div className='absolute top-0 left-0 w-full z-10'>
             <div className='container mx-auto flex justify-between
@@ -16,12 +24,12 @@ const Navbar = () => {
                     <a href="#Header" className="cursor-pointer hover:text-gray-400 no-underline">Testimonials</a>
                 </ul>
                 <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
-                <img src={menuIcon} className='md:hidden w-7 cursor-pointer' alt=""/>
+                <img onClick={() => setShowMobileMenu(true)} src={menuIcon} className='md:hidden w-7 cursor-pointer' alt=""/>
                 {/*Mobile Menu*/}
-                <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white
-                transition-all'>
+                <div className={`md:hidden ${showMobileMenu ? 'fixed w-full' : 'h-0 w-0'} right-0 top-0 bottom-0 overflow-hidden bg-white
+                transition-all`}>
                     <div className='flex justify-end p-6 cursor-pointer'>
-                        <img src={crossIcon} className='w-6 ' alt=""/>
+                        <img onClick={() => setShowMobileMenu(false)} src={crossIcon} className='w-6 ' alt=""/>
                     </div>
                     <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
                         <a href="#Header" className='px-4 py2 rounded-full inline-block '>Home</a>
