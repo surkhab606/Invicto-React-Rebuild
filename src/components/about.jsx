97c6c83ba36eceef728ad245a0f508dd4c643b08
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-16 01:28:26 -0600

    v0.17: About page development, image and grid / col text added

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import stairs from '/Untitled.png'
 
 const About = () => {
     return (
@@ -7,6 +8,29 @@ const About = () => {
             <h1 className='text-2xl sm:text-4xl font-bold mb-2'>Our <span
                 className='underline underline-offset-4 decoration-1 under font-light'>Story</span></h1>
             <p className='text-yellow-600 max-w-80 text-center mb-8'>Designed to Inspire. Built to Last.</p>
+            <div className='flex flex-col md:flex-row items-center md:items-start md:gap-20'>
+                <img src={stairs} alt="" className='w-full sm:w-1/2 max-w-lg'/>
+                <div className='flex flex-col items-center md:items-start mt-10 text-yellow-600'>
+                    <div className='grid grid-cols-2 gap-6 md:gap-10 w-full 2xl:pr-28'>
+                        <div>
+                            <p className='text-4xl font-medium text-yellow-600'>From Foundation to Finish</p>
+                            <p>Crafted with Care</p>
+                        </div>
+                        <div>
+                            <p className='text-4xl font-medium text-yellow-600'>10+</p>
+                            <p>Projects Completed</p>
+                        </div>
+                        <div>
+                            <p className='text-4xl font-medium text-yellow-600'>25,000+</p>
+                            <p>Square Feet of Quality Craftsmanship Delivered</p>
+                        </div>
+                        <div>
+                            <p className='text-4xl font-medium text-yellow-600'>Your Vision</p>
+                            <p>Our Commitment</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
