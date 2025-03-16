f64ab98055541086b610e1dd5f6665ba0cf1f634
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-16 00:36:30 -0600

    v0.12: Working on mobile formatting. Brought in menu close icons (crossIcon) and working on CSS adjustments via Tailwind. Have loved using Tailwind thus far.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import icon from '/headerlogo.png'
 import menuIcon from '/menu_icon.svg'
+import crossIcon from '/cross_icon.svg'
 
 const Navbar = () => {
     return (
@@ -15,7 +16,20 @@ const Navbar = () => {
                     <a href="#Header" className="cursor-pointer hover:text-gray-400 no-underline">Testimonials</a>
                 </ul>
                 <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Sign Up</button>
-                <img src={menuIcon} className='md:hidden w-7' alt=""/>
+                <img src={menuIcon} className='md:hidden w-7 cursor-pointer' alt=""/>
+                {/*Mobile Menu*/}
+                <div className='md:hidden fixed w-full right-0 top-0 bottom-0 overflow-hidden bg-white
+                transition-all'>
+                    <div className='flex justify-end p-6 cursor-pointer'>
+                        <img src={crossIcon} className='w-6 ' alt=""/>
+                    </div>
+                    <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
+                        <a href="#Header" className='px-4 py2 rounded-full inline-block '>Home</a>
+                        <a href="#About" className='px-4 py2 rounded-full inline-block '>About</a>
+                        <a href="#Projects" className='px-4 py2 rounded-full inline-block '>Projects</a>
+                        <a href="#Testimonials" className='px-4 py2 rounded-full inline-block '>Testimonials</a>
+                    </ul>
+                </div>
             </div>
         </div>
     )
