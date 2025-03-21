6ffc3ff493f662ab241c736fb186c5058d405fcd
30189028 <surkhab.mundi@ucalgary.ca>
2025-03-21 13:51:35 -0600

    v0.20: Header text and power statement with CSS completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from "./components/header.jsx"
 import About from "./components/about.jsx"
+import Projects from "./components/projects.jsx"
 
 const App = () => {
     return (
         <div className='w-full overflow-hidden'>
             <Header/>
             <About />
+            <Projects />
         </div>
     )
 }
---
+++ src/components/projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Projects = () => {
+    return (
+        <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full
+        overflow-hidden' id='Projects'>
+            <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center '>Projects
+                <span className="underline mx-2 underline-offset-4 decoration-1 under font-light">
+                     Completed</span></h1>
+            <p className='text-center text-yellow-600 mb-8 max-w-80 mx-auto'>
+                Excellence is our foundation.</p>
+        </div>
+
+
+    )
+}
+
+export default Projects
